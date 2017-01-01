963011466c18273940bb89428dc5349b13a50a6c
inventivetalent <inventivetalent@inventivegames.de>
2017-01-01 12:18:34 +0100

    this isn't supposed to be here yet...

--- public/v2/index.php
+++ public/v2/index.php
@@ -827,46 +827,6 @@ $app->group("/webhook", function () use ($app) {
 });
 $app->group("/metrics", function () use ($app) {
 
-    $app->get("/requests-new/:days", function ($days) use ($app) {
-        $minTime = strtotime("00:00:00 GMT" . $days . " days ago");
-        if ($minTime === false) {
-            echoData(array("error" => "invalid time frame"), 400);
-            exit ();
-        }
-
-        $cursor = metrics()->find(array("timestamp" => array('$gte' => $minTime)))->sort(array("timestamp" => 1));
-        if ($cursor->count() <= 0) {
-            echoData(array("error" => "could not find any data for the given time frame"), 400);
-            return;
-        }
-
-        $data = array();
-
-        foreach ($cursor as $item) {
-            $global = $item["global"];
-            $entry = array(
-                "timestamp" => $item["timestamp"],
-                "total" => $global["total"],
-                "unique" => $global["unique"],
-                "userAgents" => $global["userAgents"],
-                "paths" => $global["paths"]
-            );
-
-            asort($entry["userAgents"]);
-            asort($entry["paths"]);
-
-            $data[] = $entry;
-        }
-
-        usort($data, function ($a, $b) {
-            if ($a["timestamp"] === $b["timestamp"]) {
-                return 0;
-            };
-            return ($a["timestamp"] < $b["timestamp"]) ? -1 : 1;
-        });
-        echoData($data);
-    })->name("/metrics/requests");
-
     $app->get("/requests/:days", function ($days) use ($app) {
         $minTime = strtotime("00:00:00 GMT" . $days . " days ago");
         if ($minTime === false) {
